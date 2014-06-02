eb24921df9b4c496c36110c55dc9f4491fb1816c
bbbird <bbbbbird1@gmail.com>
2014-06-02 13:04:08 +0800

    upload - 課前準備

---
+++ Bird/Demo1/Demo1/BBViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBViewController.h
+//  Demo1
+//
+//  Created by Mac on 2014/5/26.
+//  Copyright (c) 2014年 iOSTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBViewController : UIViewController
+
+@end
---
+++ Bird/Demo1/Demo1/BBViewController.m
@@ -0,0 +1,29 @@
+//
+//  BBViewController.m
+//  Demo1
+//
+//  Created by Mac on 2014/5/26.
+//  Copyright (c) 2014年 iOSTeam. All rights reserved.
+//
+
+#import "BBViewController.h"
+
+@interface BBViewController ()
+
+@end
+
+@implementation BBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
